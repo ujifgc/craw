ad97de53ffd139ec20e4e712f78465ff00883398
Igor Bochkariov <ujifgc@gmail.com>
2014-04-24 10:08:52 +0400

    update extractor

--- extract.rb
+++ extract.rb
@@ -1,18 +1,20 @@
-#!/usr/bin/ruby1.9.1
-require 'logger'
+#!/usr/bin/env ruby
+require 'awesome_print'
 require 'time'
 
-OUTPUT = "V:/temp/_WATCH!"
+INPUT = ARGV[0]
+OUTPUT = ARGV[1]
+ARCHIVE = ARGV[2]
 
-log = Logger.new "#{ENV['HOME']}/extract.log"
-log << "\n\n================ " + Time.new.strftime("%Y-%m-%d %H:%M:%S") + " =================\n"
+raise ArgumentError, 'No directory provided'  unless INPUT.kind_of? String
 
-dir = ARGV[1].gsub /\\+/, '/'
+ap  "\n\n================ " + Time.new.strftime("%Y-%m-%d %H:%M:%S") + " =================\n"
 
-files = Dir.glob dir + '/**/*.rar'
+files = Dir.glob INPUT + '/*.rar'
+ap 'no files found'  unless files.any?
 
 files.each do |file|
-  log.info "found file #{file}"
+  ap "found file #{file}"
   inside = `7z l #{file}`
   movies = inside.lines.grep /\.mkv|\.mp4|\.avi|\.m4v$/
   good = false
@@ -24,25 +26,30 @@ files.each do |file|
       :size => tags[-3].strip,
     }
     good = @file[:name].match( /^(.*)\.s(\d+)e(\d+)(?:\-?e\d+)?\.(.*)$/i ) || @file[:name].match( /^(.*)\.(\d+)x(\d+)?\.(.*)$/i ) || @file[:name].match( /^(.*)\.(\d+)?\.(.*)$/i )
-    
-    if good
-      log.info "detected movie #{movie.strip}"
 
-      oldname = File.join(OUTPUT, @file[:name])
-      if File.exists? oldname
-        oldsize = File.size oldname
-        good = false  if oldsize.to_i == @file[:size].to_i
+    if good
+      ap "detected movie #{movie.strip}"
+
+      oldsize = 0
+      oldname = ''
+      [OUTPUT, ARCHIVE].each do |dirname|
+        pathname = File.join(dirname, @file[:name])
+        if File.exists? pathname
+          oldname = pathname
+          oldsize = File.size pathname
+          good = false  if oldsize.to_i == @file[:size].to_i
+          break
+        end
       end
+
       if good
-        log.info "invoking 7z extractor, new size: #{@file[:size]}"
-        puts "invoking 7z extractor, new size: #{@file[:size]}"
-        puts "7z e #{file} -aoa -o#{OUTPUT}"
-        log << `7z e #{file} -aoa -o#{OUTPUT}`
+        ap "invoking 7z extractor, new size: #{@file[:size]}"
+        ap "7z e #{file} -aoa -o#{OUTPUT}"
+        ap `7z e #{file} -aoa -o#{OUTPUT}`
       else
-        log.info "skipping existing file size: #{oldsize}"
+        ap "skipping existing file size: #{oldsize} @ #{oldname}"
       end
     end
-    log << "\n"
 
   end
 end
