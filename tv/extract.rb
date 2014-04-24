e44132b9df885b1d6543136c96df7a5643187bed
Igor Bochkariov <ujifgc@gmail.com>
2014-04-24 11:42:38 +0400

    refactor extractor

--- tv/extract.rb
+++ tv/extract.rb
@@ -1,55 +1,49 @@
 #!/usr/bin/env ruby
-require 'awesome_print'
 require 'time'
 
-INPUT = ARGV[0]
-OUTPUT = ARGV[1]
-ARCHIVE = ARGV[2]
+INPUT, OUTPUT, ARCHIVE = ARGV
+
+ARCHIVE_GLOB = '*.rar'
+MOVIE_EXTNAMES = /(?:\.mkv|\.mp4|\.avi|\.m4v)$/i
 
 raise ArgumentError, 'No directory provided'  unless INPUT.kind_of? String
 
-ap  "================ " + Time.new.strftime("%Y-%m-%d %H:%M:%S") + " ================="
+puts  "\n================ " + Time.new.strftime("%Y-%m-%d %H:%M:%S") + " ================="
 
-files = Dir.glob INPUT + '/*.rar'
-ap 'no files found'  unless files.any?
+files = Dir.glob File.join(INPUT, ARCHIVE_GLOB)
+puts 'no files found'  unless files.any?
 
 files.each do |file|
-  ap "found file #{file}"
-  inside = `7z l #{file}`
-  movies = inside.lines.grep /\.mkv|\.mp4|\.avi|\.m4v$/
-  good = false
-  @file = {}
-  movies.each do |movie|
-    tags = movie.split( /\s+/ )
-    @file = {
-      :name => tags[-1].strip,
-      :size => tags[-3].strip,
-    }
-    good = @file[:name].match( /^(.*)\.s(\d+)e(\d+)(?:\-?e\d+)?\.(.*)$/i ) || @file[:name].match( /^(.*)\.(\d+)x(\d+)?\.(.*)$/i ) || @file[:name].match( /^(.*)\.(\d+)?\.(.*)$/i )
+  puts "found file #{file}"
+  listing = `7z l #{file}`
 
-    if good
-      ap "detected movie #{movie.strip}"
+  listing.lines.grep(MOVIE_EXTNAMES).each do |movie|
+    tags = movie.split /\s+/
+    filename = tags[-1].strip
+    filesize = tags[-3].strip
 
-      oldsize = 0
-      oldname = ''
-      [OUTPUT, ARCHIVE].each do |dirname|
-        pathname = File.join(dirname, @file[:name])
-        if File.exists? pathname
-          oldname = pathname
-          oldsize = File.size pathname
-          good = false  if oldsize.to_i == @file[:size].to_i
-          break
-        end
-      end
+    puts "detected movie #{movie.strip}"
 
-      if good
-        ap "invoking 7z extractor, new size: #{@file[:size]}"
-        ap "7z e #{file} -aoa -o#{OUTPUT}"
-        ap `7z e #{file} -aoa -o#{OUTPUT}`
-      else
-        ap "skipping existing file size: #{oldsize} @ #{oldname}"
+    oldsize = 0
+    oldname = ''
+    skip = nil
+    [OUTPUT, ARCHIVE].each do |dirname|
+      pathname = File.join(dirname, filename)
+      if File.file? pathname
+        oldname = pathname
+        oldsize = File.size pathname
+        skip = true if oldsize.to_i == filesize.to_i
+        break
       end
     end
 
+    if skip
+      puts "skipping existing file size: #{oldsize} @ #{oldname}"
+      next
+    end
+
+    puts "invoking 7z extractor, new size: #{filesize}"
+    puts "7z e #{file} -aoa -o#{OUTPUT}"
+    puts `7z e #{file} -aoa -o#{OUTPUT}`
   end
 end
